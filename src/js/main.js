05fb3ea74676b885a3e15f95a7041bbfbd59a7a3
Noah Feng <reachanotherlevel@gmail.com>
2016-07-29 13:05:20 +0200

    Added things

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+
+	require('../styl/main.styl');
+	require('../styl/app.styl');
+	require('../styl/mobile.styl');
+	require('../styl/toold.styl');
+	require('gsap');
+
+	var ApplicationRouter = require('./routers/application_router');
+	var router = new ApplicationRouter();
+	Backbone.history.start();
+});
