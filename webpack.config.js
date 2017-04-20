7d5114a99d14640f51b691c94542dca9da4b945d
Noah Fenghom <noahfenghom@gmail.com>
2017-04-21 00:10:17 +0200

    Webpack => Webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,86 @@
-var webpack = require('webpack');
-var path = require("path");
-// load asynchron
-var AsyncModulePlugin = require('async-module-loader/plugin');
-// compilation progress bar
-var ProgressBarPlugin = require('progress-bar-webpack-plugin');
-// extract css, json, ... from app.js
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+const extractSass = new ExtractTextPlugin({
+    filename: "../css/[name].css",
+    disable: false
+});
 module.exports = {
-    entry:{
+    context: path.resolve(__dirname, './'),
+    entry: {
         main: './src/js/main.js',
     },
     output: {
-        path: path.join(__dirname, "/public/"),
-        publicPath: "/public/",
-        filename: "/js/app.js",
-        chunkFilename: "[name].js",
+        path: path.resolve(__dirname, './dist/js'),
+        filename: '[name].bundle.js',
     },
     module: {
-        loaders: [
-            /*{ test: /\.css$/, loader: "style!css" },*/
-            { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')},
-            { test: /\.styl$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader')},
-            /*{ test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader'},*/
-            { test: /\.html$/, loaders: ['mustache'/*, 'html'*/]},
-            { test: /\.json$/, loader: 'json-loader?name=json/[name].[ext]?[hash]' },
-            /*{ test: /\.json$/, loader: 'file?name=json/[name].[ext]?[hash]' },*/
-            // { test: /\.mp3$/, loader: 'file?name=audio/[name].[ext]' },
-            /*{ test: /\.srt$/, loader: 'file?name=subtitles/[name].[ext]' },*/
-            // { test: /\.srt$/, loader: 'raw-loader!srt-loader' },
-            { test: /\.(jpe?g|png|gif)$/i, loaders: ['file?hash=sha512&digest=hex&name=images/export/[name].[ext]','image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false']},
-            { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/, loader : 'file-loader?name=fonts/[name].[ext]'}
-        ]
+        rules: [
+        {
+            test: /\.sass$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader?indentedSyntax=sass"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        },
+        {
+            test: /\.styl$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "stylus-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        },
+        {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                use: "css-loader",
+                fallback: "style-loader"
+            }),
+        },
+        {
+            test: /\.html$/,
+            use: "mustache-loader"
+        },
+        // {
+        //   test: /\.json$/,
+        //   use: 'json-loader' // NOT NEEDED ANYMORE https://webpack.js.org/guides/migrating/#json-loader-is-not-required-anymore
+        // },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            use: ['file?hash=sha512&digest=hex&name=images/export/[name].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+            ]
+        },
+        {
+          test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+          use: 'file-loader?name=fonts/[name].[ext]'
+        }]
     },
-    plugins : [
-        new ExtractTextPlugin("/css/[name].css"),
-    	new ProgressBarPlugin(),
-        new webpack.ProvidePlugin({
-            $ : "jquery",
-            jQuery: 'jquery',
-            Backbone : "backbone",
-            _ : "underscore",
-        }),
-        new AsyncModulePlugin(),
-    ]
+  plugins: [
+    extractSass, // see first few line to see the definition and the output
+    new webpack.ProvidePlugin({
+        $ : "jquery",
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Backbone : "backbone",
+        _ : "underscore",
+    }),
+  ]
 };
+
+// Tuto: https://blog.madewithenvy.com/getting-started-with-webpack-2-ed2b86c68783
+// Doc: https://webpack.js.org
+
+// Note: __dirname refers to the directory where this webpack.config.js lives, which in this blogpost is the project root.
+// Note: in the output, [name] stands for the entry name of your entry, in this case it's main
